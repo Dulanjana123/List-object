ca8479492525de2ffc9c1cdf5a2d375b7f62bd53
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2022-11-07 02:29:18 +0530

    Add project files.

---
+++ List object/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_object
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int type list
+            List<int> nums = new List<int>();
+
+            nums.Add(1);
+            nums.Add(2);
+            nums.Add(4);
+
+            //object type list
+            List<object> objlist = new List<object>();
+
+            objlist.Add(new  { name = "dulanjana", age = 18, address = "Polgolla" });
+            objlist.Add(new { name = "amila", age = 20, address = "nawayalathanna" });
+
+            foreach(object obj in objlist)
+            {
+                Console.WriteLine(obj + "\n");
+            }
+
+            //create new linked list from subjects class
+            LinkedList<Subjects> subjects = new LinkedList<Subjects>();
+
+            subjects.AddLast(new Subjects { subject = "cs", semester = 1 });
+            subjects.AddLast(new Subjects { semester = 2, subject = "dsa" });
+
+            foreach (Subjects s in subjects)
+            {
+                Console.WriteLine("subject = {0}" + " " + "semester = {1}\n", s.subject, s.semester);
+            }
+
+
+
+
+            
+        }
+    }
+}
---
+++ List object/Subjects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List_object
+{
+    public class Subjects
+    {
+        public string subject { get; set; }
+        public int semester { get; set; }
+
+    }
+}
